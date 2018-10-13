96f6e9598b2f3964dfc4e01de1cd7af4b5a7c2fb
Your Name <you@example.com>
2018-10-13 18:30:53 +0200

    list after create

--- core_api/views/customer.py
+++ core_api/views/customer.py
@@ -10,3 +10,7 @@ class CustomerViewset(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = serializer_class.Meta.model.objects.all()
     permission_classes = (IsAuthenticated,)
+
+    def create(self, request, *args, **kwargs):
+        super(CustomerViewset, self).create(request, *args, **kwargs)
+        return super(CustomerViewset, self).list(request, *args, **kwargs)
